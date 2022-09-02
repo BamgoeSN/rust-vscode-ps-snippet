cd8edc8d5d8b1478e599762e8bcc98d1ae59b103
BamgoeSN <60645387+BamgoeSN@users.noreply.github.com>
2022-09-02 15:54:42 +0900

    Update test

--- src/main.rs
+++ src/main.rs
@@ -87,78 +87,78 @@ mod test {
     use serde_json::{Map, Result};
 
     #[test]
-    fn generate_snippet_test() -> Result<()> {
+    pub fn generate_snippet_test() -> Result<()> {
         let code = r#"fn main() {
-        // FastIO
-        use {fastio::*, std::io::*};
-        let input_str: &str = get_input();
-        let mut scan: Scanner<_> = Scanner::tokenize(input_str);
-        let stdout = stdout();
-        let out = &mut BufWriter::new(stdout.lock());
-    
-        // FastIO Macros
-        macro_rules! next {
-            () => { scan.next() };
-            (str) => { scan.next_str() };
-            ($($t:ty) +) => { ($(scan.next::<$t>()),+) };
-        }
-        macro_rules! out { ($($arg:tt)*) => { write!(out, $($arg)*).ok(); }; }
-        macro_rules! outln { ($($arg:tt)*) => { writeln!(out, $($arg)*).ok(); }; }
-    
-        // Main
+    // FastIO
+    use {fastio::*, std::io::*};
+    let input_str: &str = get_input();
+    let mut scan: Scanner<_> = Scanner::tokenize(input_str);
+    let stdout = stdout();
+    let out = &mut BufWriter::new(stdout.lock());
+
+    // FastIO Macros
+    macro_rules! next {
+        () => { scan.next() };
+        (str) => { scan.next_str() };
+        ($($t:ty) +) => { ($(scan.next::<$t>()),+) };
     }
-    
-    mod fastio {
-        extern "C" {
-            fn mmap(addr: usize, len: usize, p: i32, f: i32, fd: i32, o: i64) -> *mut u8;
-            fn fstat(fd: i32, st
-                type Snippet = Map<String, Content>;
-    
-        pub fn get_input() -> &'static str {
-            let mut stat = [0; 20];
-            unsafe { fstat(0, (&mut stat).as_mut_ptr()) };
-            let buffer = unsafe { mmap(0, stat[6], 1, 2, 0, 0) };
-            unsafe { std::str::from_utf8_unchecked(std::slice::from_raw_parts(buffer, stat[6])) }
-        }
-    
-        pub struct Scanner<'a, I: Iterator<Item = &'a str>> {
-            it: I,
-        }
-    
-        impl<'a> Scanner<'a, std::str::SplitAsciiWhitespace<'a>> {
-            pub fn tokenize(s: &'a str) -> Self {
-                Self {
-                    it: s.split_ascii_whitespace(),
-                }
-            }
-        }
-    
-        impl<'a> Scanner<'a, std::str::Lines<'a>> {
-            pub fn lines(s: &'a str) -> Self {
-                Self { it: s.lines() }
-            }
-        }
-    
-        impl<'a, I: Iterator<Item = &'a str>> Scanner<'a, I> {
-            #[inline(always)]
-            pub fn next<T: std::str::FromStr>(&mut self) -> T {
-                self.it.next().unwrap().parse().ok().unwrap()
-            }
-            #[inline(always)]
-            pub fn next_str(&mut self) -> &'a str {
-                self.it.next().unwrap()
-            }
-            #[inline(always)]
-            pub fn next_option<T: std::str::FromStr>(&mut self) -> Option<T> {
-                self.it.next().and_then(|s| s.parse().ok())
-            }
-            #[inline(always)]
-            pub fn next_str_option(&mut self) -> Option<&'a str> {
-                self.it.next()
+    macro_rules! out { ($($arg:tt)*) => { write!(out, $($arg)*).ok(); }; }
+    macro_rules! outln { ($($arg:tt)*) => { writeln!(out, $($arg)*).ok(); }; }
+
+    // Main
+}
+
+mod fastio {
+    extern "C" {
+        fn mmap(addr: usize, len: usize, p: i32, f: i32, fd: i32, o: i64) -> *mut u8;
+        fn fstat(fd: i32, st
+            type Snippet = Map<String, Content>;
+
+    pub fn get_input() -> &'static str {
+        let mut stat = [0; 20];
+        unsafe { fstat(0, (&mut stat).as_mut_ptr()) };
+        let buffer = unsafe { mmap(0, stat[6], 1, 2, 0, 0) };
+        unsafe { std::str::from_utf8_unchecked(std::slice::from_raw_parts(buffer, stat[6])) }
+    }
+
+    pub struct Scanner<'a, I: Iterator<Item = &'a str>> {
+        it: I,
+    }
+
+    impl<'a> Scanner<'a, std::str::SplitAsciiWhitespace<'a>> {
+        pub fn tokenize(s: &'a str) -> Self {
+            Self {
+                it: s.split_ascii_whitespace(),
             }
         }
     }
-    "#;
+
+    impl<'a> Scanner<'a, std::str::Lines<'a>> {
+        pub fn lines(s: &'a str) -> Self {
+            Self { it: s.lines() }
+        }
+    }
+
+    impl<'a, I: Iterator<Item = &'a str>> Scanner<'a, I> {
+        #[inline(always)]
+        pub fn next<T: std::str::FromStr>(&mut self) -> T {
+            self.it.next().unwrap().parse().ok().unwrap()
+        }
+        #[inline(always)]
+        pub fn next_str(&mut self) -> &'a str {
+            self.it.next().unwrap()
+        }
+        #[inline(always)]
+        pub fn next_option<T: std::str::FromStr>(&mut self) -> Option<T> {
+            self.it.next().and_then(|s| s.parse().ok())
+        }
+        #[inline(always)]
+        pub fn next_str_option(&mut self) -> Option<&'a str> {
+            self.it.next()
+        }
+    }
+}
+"#;
 
         let name = "psbase";
         let base = generate_snippet(name, code);
@@ -168,6 +168,10 @@ mod test {
 
         let x = serde_json::to_string(&snippet)?;
         println!("{}", x);
+        assert_eq!(
+            x,
+            r##"{"psbase":{"body":["fn main() {","    // FastIO","    use {fastio::*, std::io::*};","    let input_str: &str = get_input();","    let mut scan: Scanner<_> = Scanner::tokenize(input_str);","    let stdout = stdout();","    let out = &mut BufWriter::new(stdout.lock());","","    // FastIO Macros","    macro_rules! next {","        () => { scan.next() };","        (str) => { scan.next_str() };","        ($(\\$t:ty) +) => { ($(scan.next::<\\$t>()),+) };","    }","    macro_rules! out { ($(\\$arg:tt)*) => { write!(out, $(\\$arg)*).ok(); }; }","    macro_rules! outln { ($(\\$arg:tt)*) => { writeln!(out, $(\\$arg)*).ok(); }; }","","    // Main","}","","mod fastio {","    extern \"C\" {","        fn mmap(addr: usize, len: usize, p: i32, f: i32, fd: i32, o: i64) -> *mut u8;","        fn fstat(fd: i32, st","            type Snippet = Map<String, Content>;","","    pub fn get_input() -> &'static str {","        let mut stat = [0; 20];","        unsafe { fstat(0, (&mut stat).as_mut_ptr()) };","        let buffer = unsafe { mmap(0, stat[6], 1, 2, 0, 0) };","        unsafe { std::str::from_utf8_unchecked(std::slice::from_raw_parts(buffer, stat[6])) }","    }","","    pub struct Scanner<'a, I: Iterator<Item = &'a str>> {","        it: I,","    }","","    impl<'a> Scanner<'a, std::str::SplitAsciiWhitespace<'a>> {","        pub fn tokenize(s: &'a str) -> Self {","            Self {","                it: s.split_ascii_whitespace(),","            }","        }","    }","","    impl<'a> Scanner<'a, std::str::Lines<'a>> {","        pub fn lines(s: &'a str) -> Self {","            Self { it: s.lines() }","        }","    }","","    impl<'a, I: Iterator<Item = &'a str>> Scanner<'a, I> {","        #[inline(always)]","        pub fn next<T: std::str::FromStr>(&mut self) -> T {","            self.it.next().unwrap().parse().ok().unwrap()","        }","        #[inline(always)]","        pub fn next_str(&mut self) -> &'a str {","            self.it.next().unwrap()","        }","        #[inline(always)]","        pub fn next_option<T: std::str::FromStr>(&mut self) -> Option<T> {","            self.it.next().and_then(|s| s.parse().ok())","        }","        #[inline(always)]","        pub fn next_str_option(&mut self) -> Option<&'a str> {","            self.it.next()","        }","    }","}"],"prefix":"psbase"}}"##
+        );
 
         Ok(())
     }
